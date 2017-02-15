2da289584b52ad8da3ec4c8460a5d4e65974c223
amit <amitguru77@gmail.com>
2017-02-15 10:55:11 -0800

    second commit

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,7 @@
 class StoreController < ApplicationController
 
+	# skip_before_action :authorize
+
 	# folloing two lines are added according to page no 149
 	 include CurrentCart
 	 before_action :set_cart
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,27 @@
 Rails.application.routes.draw do
+
+  # get 'admin' => 'admin#index'
+  # controller :sessions do
+  # get 'login' => :new
+  # post 'login' => :create
+  # delete 'logout' => :destroy
+  #  end
+  get 'admin/index'
+
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  resources :users
   resources :orders
   resources :line_items
   resources :carts
-	root 'store#index', as:'store_index'
-  # get 'store/index'
 
-  resources :products
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'store#index', as:'store_index'
+
+  resources :products do
+ 	get :who_bought, on: :member
+  end
+
+    # For details on the DSL available within this file, see # http://guides.rubyonrails.org/routing.html
 end
